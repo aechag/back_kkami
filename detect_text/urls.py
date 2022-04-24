dc61d0ce89a38d1e50d6ed0204231a8ea13ccf7d
aechag <aechag99@gmail.com>
2022-04-24 00:07:40 +0000

    getUserImg

--- detect_text/urls.py
+++ detect_text/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -5,4 +7,4 @@ from detect_text import views
 
 urlpatterns = [
     path('detectText/', views.detectText),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- detect_text/views.py
+++ detect_text/views.py
@@ -7,20 +7,37 @@ import cv2
 import easyocr
 from pylab import rcParams
 from IPython.display import Image
+#import magic
+import imghdr
+
+from detect_text.forms import UploadFileForm
+
+from django.views.decorators.csrf import csrf_exempt
 
 rcParams['figure.figsize'] = 8, 16
 reader = easyocr.Reader(['en'])
 
+@csrf_exempt
 def detectText(request):
-    textfromimg = ''
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-    file_name = "detect_text/aspro.jpg"
-    output = reader.readtext(file_name)
-    i = 0
-    size = len(output)
-    while i < size:
-        textfromimg = textfromimg + ' ' + output[i][1]
-        print(output[i][1])
-        i = i + 1
+    if request.method == 'POST':
+        # form = UploadFileForm()
+        textfromimg = ''
+        os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-    return HttpResponse(textfromimg)
+        # file_name = "detect_text/aspro.jpg"
+        file_name = request.FILES['image']
+        data = file_name.seek(0)
+        data0 = file_name.read()
+
+        output = reader.readtext(data0)
+        i = 0
+        size = len(output)
+        while i < size:
+            textfromimg = textfromimg + ' ' + output[i][1]
+            # print(output[i][1])
+            i = i + 1
+
+        return HttpResponse(textfromimg)
+
+    else:
+        return HttpResponse('its a get')
